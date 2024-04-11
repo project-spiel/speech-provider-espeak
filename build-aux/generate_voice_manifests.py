ee9f8867cdcade0e27e599f548ca568e5de1f575
Eitan Isaacson <eitan@monotonous.org>
2024-04-11 10:39:29 -0700

    Make voice flatpak extensions

---
+++ build-aux/generate_voice_manifests.py
@@ -0,0 +1,52 @@
+import pathlib
+import json
+import urllib.request
+import urllib.parse
+
+MANIFEST_TEMPLATE = '''{{
+  "app-id": "org.espeak.Speech.Provider.Voice.{escaped_name}",
+  "runtime": "org.espeak.Speech.Provider",
+  "runtime-version": "master",
+  "sdk": "org.freedesktop.Sdk//23.08",
+  "build-extension": true,
+  "modules": [
+    {{
+      "name": "espeak-ng",
+      "buildsystem": "simple",
+      "build-commands": [
+        "install -D -m644 \\"espeak-ng-data/voices/\\!v/{name}\\" \\"${{FLATPAK_DEST}}/voices/{name}\\""
+      ],
+      "sources": [
+        {{
+          "type": "git",
+          "url": "https://github.com/espeak-ng/espeak-ng.git",
+          "tag": "1.51.1",
+          "commit": "34762a2b9621d3643e67a00642984c21f0626bdc",
+          "x-checker-data": {{
+            "type": "git",
+            "tag-pattern": "^([\\d.]+)$"
+          }}
+        }}
+      ]
+    }}
+  ]
+}}
+'''
+
+response = urllib.request.urlopen('https://api.github.com/repos/espeak-ng/espeak-ng/git/trees/1.51.1?recursive=1')
+data = json.load(response)
+
+build_cmds = []
+f = filter(lambda d: d["path"].startswith("espeak-ng-data/voices/!v/"), data["tree"])
+for obj in f:
+  name = pathlib.Path(obj["path"]).name
+  escaped_name = urllib.parse.quote(name.replace(" ", "_"))
+  manifest = f"voices/org.espeak.Speech.Provider.Voice.{escaped_name}.json"
+  mf = open(manifest, "w")
+  mf.write(MANIFEST_TEMPLATE.format(name=name, escaped_name=escaped_name))
+  mf.close()
+  build_cmds.append(f'flatpak-builder --force-clean --repo=repo build-dir "{manifest}"')
+
+print("#!/bin/sh")
+print("# Voice extension build script")
+print(' &&\n'.join(build_cmds))
